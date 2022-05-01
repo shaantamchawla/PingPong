f0b6ded7b4952e7a2bc9dc09773082e5761c6756
shaantamchawla <shaantamc@berkeley.edu>
2022-05-01 15:30:49 -0700

    .

--- Pico/main.py
+++ Pico/main.py
@@ -1,1 +1,13 @@
-## Motor drive code lives here
+'''
+MicroProfessors
+05/01/2022
+
+Continuously listen for motor commands from Pi Zero over UART rx / tx
+Use two cores on Pico to drive pairs of motors simultaneously at a rate of FREQ - e.g. North and South motor, East and West motor
+
+Each core drives
+
+Listen for motor commands from Pi Zero over UART rx / tx
+Continuously listens on an async rx / tx channel for computed motor commands.
+Will listen at a rate of FREQ and then drive motors at a rate of FREQ
+'''
