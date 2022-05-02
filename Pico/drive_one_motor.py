38f54efcce452392c61c4f8582628e7d163d0b77
shaantamchawla <shaantamc@berkeley.edu>
2022-05-01 21:10:51 -0700

    added multitasking skeleton code for Pico

--- Pico/drive_one_motor.py
+++ Pico/drive_one_motor.py
@@ -3,13 +3,17 @@ MicroProfessors
 04/26/2022
 
 Continuous rotation script for 1 motor
+
+Now testing dual core for multitasking — 1 for motor drive, 1 for toggle LED
+Dual core basics: https://circuitdigest.com/microcontroller-projects/dual-core-programming-on-raspberry-pi-pico-using-micropython
 '''
 
 from machine import Pin, Timer
 import time
+import _thread
 
 # Step info
-FREQ = 480 # Hz
+FREQ = 500 # Hz
 
 # Define stepper motor GPIO connections:
 DIR_GPIO = 17
@@ -26,14 +30,38 @@ CW = 0
 # Set spin direction CW or CCW
 DIR.value(CCW)
 
+## Threading for dual core
+sLock = _thread.allocate_lock()
+
+# LED
+led = Pin(25, Pin.OUT)
+led.toggle()
+
+def CoreTask():
+    Timer().init(freq=FREQ, mode=Timer.PERIODIC, callback=spin_motor)
+    
+    while True:
+        sLock.acquire()
+        print('spinning..')
+        led.toggle()
+        time.sleep(0.05)
+        print("Exiting from the 2nd Thread")
+        sLock.release()
+
 def spin_motor(timer):
     PULSE.value(1)
     PULSE.value(0)
 
 def main():
-    led = Pin(25, Pin.OUT)
-    led.toggle()
+    _thread.start_new_thread(CoreTask, ())
     
-    Timer().init(freq=FREQ, mode=Timer.PERIODIC, callback=spin_motor)
+    while True:
+        # We acquire the semaphore lock
+        sLock.acquire()
+        print("Entered into the main Thread")
+        led.toggle()
+        time.sleep(0.05)
+        print("Toggling")
+        sLock.release()
         
 main()
