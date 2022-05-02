a2f7317270ffa45c8bb11ee590303e2bb64f4831
shaantamchawla <shaantamc@berkeley.edu>
2022-05-01 19:38:01 -0700

    Direction control

--- Pico/drive_one_motor.py
+++ Pico/drive_one_motor.py
@@ -9,7 +9,7 @@ from machine import Pin, Timer
 import time
 
 # Step info
-step_delay = 0.005 # in seconds
+step_delay = 0.001 # in seconds - min delay allowable at full step
 
 # Define stepper motor GPIO connections:
 DIR_GPIO = 17
@@ -18,9 +18,13 @@ PULSE_GPIO = 16
 # Declare pins as output
 DIR = Pin(DIR_GPIO, Pin.OUT)
 PULSE = Pin(PULSE_GPIO, Pin.OUT)
+
+## Direction control
+CCW = 1
+CW = 0
     
 # Set spin direction CW or CCW
-DIR.value(0)
+DIR.value(CCW)
 
 def main():
     led = Pin(25, Pin.OUT)
@@ -28,8 +32,8 @@ def main():
     
     while True:
         PULSE.value(1)
-        time.sleep(6.0)
+        time.sleep(step_delay)
         PULSE.value(0)
-        time.sleep(6.0)
+        time.sleep(step_delay)
         
 main()
