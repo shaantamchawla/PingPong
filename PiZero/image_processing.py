c3ef66ec6d1d0dd43ec1c077356a5e2c05d9f8c9
Lepton170 <47232320+devlint22@users.noreply.github.com>
2022-05-04 13:53:12 -0700

    Update image_processing.py

--- PiZero/image_processing.py
+++ PiZero/image_processing.py
@@ -4,3 +4,92 @@
 	## we are using circle detect -> determine height / x / y -> compute motor control commands
 	## then, send motor command over UART to pico
 	## pico takes care of motor driving
+
+import cv2 as cv
+import numpy as np
+import picamera as pc
+import time
+import os
+import math
+
+
+class closed_loop_ctrl:
+    def __init__(self):
+        self.cam = pc.PiCamera()
+        self.cam.framerate = 30
+        self.res = [320,240]
+        self.cam.resolution = (self.res[0],self.res[1])
+        self.deg_rng = 10   #symmetric (value for one side)
+        self.Lp = 0.160     #platform sidelength
+
+    def runout(self):
+        r = self.Lp/0.8
+        period = 5
+        xt = lambda t: r*math.cos(2*math.pi*(t/period))
+        yt = lambda t: r*math.sin(2*math.pi*(t/period))
+        x0,y0 = [0,0]
+        tend = 2*period
+        t0 = time.time()
+        t = t0
+        x = np.array([[x0,y0]])
+        e_sum = 0
+        while t < t0+tend:
+            tp = t
+            t = time.time()
+            dt = t-tp
+            xb,yb,rb = self.process_frame(self.get_frame())
+            v = np.subtract([xb,yb],x[-1,:])/dt
+            x = np.append(x,[[xb,yb]],0)
+            e_sum = e_sum + np.subtract([xt,yt],[xb,yb])
+
+            thout,phout = self.controller([xb,yb],[xt,yt],v,e_sum)
+
+
+
+
+
+
+    def controller(x,xt,v,e_sum=[0,0],K=[6,0,-1]):
+        '''
+        symmetric PID controllers in 2D. Default to no integration of error
+        '''
+        Kp,Ki,Kd = K
+        trng = [math.radians(-1*self.deg_rng),math.radians(self.deg_rng)]
+        g = 9.802
+        dir_t = np.subtract(xt,x)               # vector TO target
+        xy_dir = math.atan2(dir_t[1],dir_t[0])  # atan2 preserves quadrant
+#        xy_dir = xy_dir+math.pi*2 % math.pi*2
+        a_des = [Kp*err + Ki*int + Kd*vel for err,int,vel in zip(dir_t,v,e_sum)]
+        arng = [g*math.sin(trng[0]),g*math.sin(trng[1])]
+        ax = min(max(a_des[0],arng[0]),arng[1])
+        ay = min(max(a_des[1],arng[0]),arng[1])
+
+        thout = math.asin(ax/g)     # plant
+        phout = math.asin(ay/g)
+        return thout,phout          # absolute plate angles (theta --> x accel, phi --> y accel)
+
+    def get_frame(self, prev=False):
+        cf = np.empty((res[0],res[1],3), dtype=np.uint8)    # 24 bit depth
+        self.cam.capture(cf,'rgb')
+        if prev:
+            cv.imshow('frame',cf)
+            time.sleep(1)
+            cv.destroyAllWindows()
+        return cf
+
+    def process_frame(self, cf, K_rgb=[0,0.4,1], prev=False):
+        pf = np.empty(shape(cf),dtype=np.uint8)
+        for k in range(0,3):
+            pf[:,:,k] = K_rgb[k]*cf[:,:,k]
+        pf = cv.cvtColor(pf, cv.COLOR_BGR2GRAY)
+        pf = cv.convertScaleAbs(pf, alpha=3, beta=0)
+        pf = cv.blur(pf, (4,4))
+        circles = cv.HoughCircles(pf, cv.HOUGH_GRADIENT, 1, 100, param1 = 50, param2 = 40, minRadius=50,maxRadius=100)
+        circles = np.uint32(np.around(circles))
+        num_circ = 1
+        dat = circles[0]
+        if prev:
+            cv.imshow('frame',pf)
+            time.sleep(1)
+            cv.destroyAllWindows()
+        return dat
